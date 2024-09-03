97e7cee652e554ad1d763c7783894d8bbbdb6ddd
Meliketoaste <emilnord77@gmail.com>
2024-09-03 13:05:47 +0200

    ferdig

---
+++ min-kalkulator/app.js
@@ -0,0 +1,50 @@
+// Først henter vi ut alle elementene vi skal bruke fra HTML
+const txtTall1 = document.querySelector("#tall1")
+const txtTall2 = document.querySelector("#tall2")
+const btnPluss = document.querySelector("#pluss")
+const btnMinus = document.querySelector("#minus")
+const btnDivide = document.querySelector("#divide")
+const btnMultiply = document.querySelector("#multiply")
+const svarDiv = document.querySelector("#svar")
+
+btnPluss.onclick = function() {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 + tall2
+    svarDiv.innerHTML = sum
+}
+
+
+btnMinus.onclick = function() {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 - tall2
+    svarDiv.innerHTML = sum
+}
+
+
+btnDivide.onclick = function() {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 / tall2;
+    svarDiv.innerHTML = sum;
+}
+
+
+btnMultiply.onclick = function() {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 * tall2;
+    svarDiv.innerHTML = sum;
+}
+
+/*
+    En alternativ måte å skrive en funksjon er å lage en "arrow function", slik:
+        () => { }
+    Dette btyr det samme som å skrive
+        function() { }
+        men det er bare en slags kortere måte å lage en funksjon, f.eks:
+        btnpluss.onclick = () => {
+        ...
+        }
+*/
